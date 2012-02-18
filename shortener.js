2decee27a8b6ccf655863c19e4b6facf128247a3
Hiroki Kuzuno <kuzuno@gmail.com>
2012-02-19 04:35:27 +0900

    support bit.ly rest url

--- shortener.js
+++ shortener.js
@@ -26,9 +26,38 @@ Shortener.prototype.google = function(longurl, key, callback) {
     res.on('end', function(e) {
       callback(JSON.parse(data));
     });
-  })
+  });
 
   var payload = '{"longUrl":"' + longurl + '", "key":"' + key + '"}';
   req.write(payload);
+  req.end();
+};
+
+Shortener.prototype.bitly = function(longurl, user, key, format, callback) {
+  var options = {
+    host: 'api-ssl.bitly.com',
+    port: 443,
+    path: '/v3/shorten?login=' + user + 
+      '&apikey=' + key + 
+      '&longUrl=' + longurl + 
+      '&format=' + format,
+    method: 'GET'
+  };
+
+  var req = https.request(options, function(res) {
+    if (res.statusCode != 200)
+      throw new Error("HTTP Status Code is not 200: " + res.statusCode);
+    var data = '';
+    res.on('data', function(d) {
+      data += d;
+    });
+    res.on('error', function(e) {
+      console.error(e);
+    });
+    res.on('end', function(e) {
+      callback(JSON.parse(data));
+    });
+  });
+
   req.end();
 };
